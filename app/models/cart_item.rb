af84703a1333d30db8b37597f99c363726765226
ting <ttzhenng@gmail.com>
2021-05-06 13:18:53 -0400

    Change Cart_Item to Cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,4 @@
-class CartItem < ApplicationRecord
-    belongs_to :user
-    belongs_to :product
+class Cart < ApplicationRecord
+    has_many :line_items, dependent: :destroy
+    has_many :items, through: :line_items
 end
