6f1671df638cac0b90b20a24c201698b5e572c90
ting <ttzhenng@gmail.com>
2021-05-06 13:25:46 -0400

    Change associations in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :products, through: :cart_items
+  has_many :products, through: :cart 
   has_many :comments, through: :products
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
