36cb95f08793b42c8c9f8c292ddf2afd34126ec1
ting <ttzhenng@gmail.com>
2021-05-08 14:24:00 -0400

    Changes to Store index.html

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    @categories = Catergory.order(:name)
+    @categories = Category.order(:name)
   end
 end
