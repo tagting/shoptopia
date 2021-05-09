db88e5d552f5d628f886e4207551e37794bc688d
ting <ttzhenng@gmail.com>
2021-05-09 16:21:23 -0400

    seeded comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :products
+    belongs_to :product
     has_many :users, through: :products
+
+
+
+validates :description, length: { maximum: 1500, too_long: "%{count} characters is the maximum allowed. "}
 end
